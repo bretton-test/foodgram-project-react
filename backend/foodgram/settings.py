fa148a2d2feb80f8ed9a9e8ea83c2030da3f3854
Алексей Долганов <dolganovAlex@yandex.ru>
2023-04-02 21:30:06 +0500

    action-deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,13 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    'django-insecure-6&#*#e4u)6ls6onvdl)t4vl_)8d1e+=^$k+ws6fkty#9$zujj_'
-)
+SECRET_KEY = (os.getenv('SECRET_KEY', 'default_secret_key'))
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*'), '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
